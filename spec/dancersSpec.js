6d656019bbcede6bc027336e60342708c7c02e42
Will Trinh <williamltrinh@gmail.com>
2021-03-16 11:44:00 -0700

    Add tests

--- spec/dancersSpec.js
+++ spec/dancersSpec.js
@@ -18,6 +18,8 @@ describe('blinkyDancer', function() {
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
+
+
   describe('dance', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
@@ -32,3 +34,26 @@ describe('blinkyDancer', function() {
     });
   });
 });
+
+describe('shakerDancer', function() {
+
+  var timeBetweenSteps = 100;
+  var shakerDancer;
+
+  beforeEach(function() {
+    shakerDancer = new ShakerDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(shakerDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a class that makes it shake on mousehover', function() {
+    expect(shakerDancer.$node).hasClass('shaking').to.equal(true);
+  });
+
+  it('should have an eject property', function() {
+    expect(shakerDancer.$node).to.have.property('eject');
+  });
+});
+
